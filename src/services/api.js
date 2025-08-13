99ec0fa6f11c9731edf7ceaba65ec5017ce574a1
sushanth-shetty11 <161237159+sushanth-shetty11@users.noreply.github.com>
2025-08-13 23:08:56 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/messages";
+const API_URL = "https://rapidquest-backend-0rhj.onrender.com/api/messages";
 
 export const getConversations = () => axios.get(`${API_URL}/conversations`);
 export const getMessages = (id) => axios.get(`${API_URL}/${id}`);
